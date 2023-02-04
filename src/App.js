e30b607eadea8cda35d23a37025c8c91a8d7885c
AVINASH NAGAR <119407873+hiddencoder007@users.noreply.github.com>
2023-02-04 20:20:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Header from './Header'
+import Mid from './Mid';
+function App() {
+  return ( 
+    <>
+    <Header/>
+    <Mid/>
+    <Footer/>
+    
+    </>
+   );
+}
+
+export default App;
---
+++ src/Mid.js
@@ -0,0 +1,86 @@
+function Mid() {
+    return ( 
+        <>
+        <section id="banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <h4>Banner Title</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque rem libero ex, inventore necessitatibus aperiam consequuntur expedita minus aspernatur ducimus.</p>
+            <button class="btn btn-success">More details</button>
+            </div>
+            <div class="col-md-6"><img src="media/about-us.png" alt="" class="img-fluid mx-auto d-block" id="img-banner"/></div>
+          </div>
+        </div>
+        <img src="media/border1.png" alt=""/>
+      </section>
+
+<section id="services">
+<div class="container pb-3">
+  <div class="row">
+    <h2>Services</h2>
+    <div class="col-md-3">
+      <div class="card">
+      <img src="media/linkedin-icon.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">More Details</a>
+      </div>
+    </div>
+  </div>
+    <div class="col-md-3">  <div class="card">
+      <img src="media/instagram-icon.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">More Details</a>
+      </div>
+    </div></div>
+    <div class="col-md-3">  <div class="card">
+      <img src="media/snapchat-icon.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">More Details</a>
+      </div>
+    </div></div>
+    <div class="col-md-3">  <div class="card">
+      <img src="media/twitter-icon.png" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">More Details</a>
+      </div>
+    </div></div>
+  </div>
+</div>
+
+</section>
+
+<section id="testi">
+        <div class="container pt-3 pb-3">
+          <div class="row">
+            <h2>Testinominal</h2>
+            <div class="col-md-3">
+              <img src="media/building.png" alt=""/>
+              <p><span><i class="bi bi-quote"></i></span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime laborum rem ducimus magnam quis modi numquam totam neque, eum labore.<i class="bi bi-quote"></i></p>
+              <p>Indigo pvt. LTD.</p>
+            </div>
+            <div class="col-md-3"><img src="media/building.png" alt=""/>
+              <p><span><i class="bi bi-quote"></i></span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime laborum rem ducimus magnam quis modi numquam totam neque, eum labore.<i class="bi bi-quote"></i></p>
+              <p>IBM PVT LTD.</p></div>
+            <div class="col-md-3"><img src="media/building.png" alt=""/>
+              <p><span><i class="bi bi-quote"></i></span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime laborum rem ducimus magnam quis modi numquam totam neque, eum labore.<i class="bi bi-quote"></i></p>
+              <p>TCS.</p></div>
+            <div class="col-md-3"><img src="media/building.png" alt=""/>
+              <p><span><i class="bi bi-quote"></i></span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime laborum rem ducimus magnam quis modi numquam totam neque, eum labore.<i class="bi bi-quote"></i></p>
+              <p>METLIFE.</p></div>
+          </div>
+        </div>
+      </section>
+</>
+     );
+}
+
+export default Mid;
